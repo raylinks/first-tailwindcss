1def9954d3423e894436cdf949ccdd170db7ce15
Raymond Rilwan <olawuyirilwan98@gmail.com>
2021-03-27 18:20:02 +0100

    background image

---
+++ src/components/Features.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="antialised mt-20 max-w-2xl mx-auto  bg-blue-100   rounded-2xl flex flex-col justify-center py-8">
+          <!-- <div class="relative py-20 sm:max-w-xl sm:mx-auto">
+        <p class="text-yellow-500 font-2xl font-semibold">These below are our services</p>
+          </div> -->
+    </div>
+</template>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,12 @@
 <template>
- <div class="antialised text-gray-800 min-h-screen bg-yellow-100 py-6  flex flex-col justify-center sm:py-12">
+<div class="container w-3/4 mx-auto my-4  min-h-screen px-4 py-4 bg-no-repeat h-40 bg-center" style="background-image: url('images/devs.jpg')">
+</div>
+
+
+
+
+
+ <!-- <div class="antialised text-gray-800 min-h-screen bg-yellow-100 py-6  flex flex-col justify-center sm:py-12">
    <div class="relative py-3 sm:max-w-xl sm:mx-auto">
     <div class="relative px-4 py-10 bg-white  shadow-lg sm:rounded-3xl sm:p-12">
         <h1 class="text-3xl font-semibold text-indigo-600">welcome to my first css</h1>
@@ -7,14 +14,14 @@
         <a href="" class="inline-block bg-indigo-600 text-white px-6 py-2 mt-4 rounded-lg hover:bg-indigo-800 uppercase"> Learn more</a>
     </div>
   </div> 
- </div> 
+ </div>  -->
 
-  <div class="home flex justify-center flex-col items-center">
+  <!-- <div class="home flex justify-center flex-col items-center">
       <h1 class="text-5xl">welcome</h1>
       <div class="flex flex-col items-center bg-yellow-400 w-1/3 p-10 rounded-lg shadow-xl">
        <img class="h-64 w-64 rounded-full object-cover"  src="https://source.unsplash.com/random" alt="" />
       <h5 class="font-2xl font-semibold">some image information</h5>
       <P class="text-center mt-4 "><span class="text-purple-900 font extrabold"> i love this language</span> it is interesting </P> 
       </div>
-  </div>
+  </div> -->
 </template>
